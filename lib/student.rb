505a39ad1db198fc4d8e86fc58aac3a1cfe6b4f4
soohoo05 <tyler.soohoo@gmail.com>
2018-09-21 09:00:53 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+attr_accessor :first_name, :last_name
+
+def initialize
+  @knowledge=[]
+end
+
+def learn(aKnowledge)
+  @knowledge << aKnowledge
+end
+
+def knowledge
+  @knowledge
+end
 
 end
