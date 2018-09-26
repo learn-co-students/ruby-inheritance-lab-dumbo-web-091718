cd90ea2166eaead08e7d0e4bca80193f248be763
github username <github email address>
2018-09-26 00:52:30 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
 
-end
+  attr_accessor :array
+
+  def initialize()
+    @array = []
+  end
+
+
+
+   def learn(string)
+    # string of knowledge and adds it to the student's knowledge array
+    @array << string
+   end
+
+ end
