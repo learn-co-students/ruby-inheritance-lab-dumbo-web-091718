380752eae2c100a02ed61065c40d4e867098dbf9
Brian Di Rito <bdirito91@gmail.com>
2019-02-23 17:03:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User
-
+  attr_reader :knowledge
+  attr_accessor :first_name, :last_name
+  
+  @@all = []
+  
+  def initialize
+    @knowledge = []
+    @@all << self
+  end
+  
+  def learn(str)
+    @knowledge << str
+  end
 end
